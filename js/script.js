711fd15f3f85336e590bd64514ec1ad8fb3ba017
Jaron <jjkenyon777@gmail.com>
2021-09-04 11:09:21 +0700

    added notes to js

--- js/script.js
+++ js/script.js
@@ -1,24 +1,34 @@
 var currentDayDisplay = document.getElementById("currentDay");
+var currentHour = moment().hour();
 
+// Displays the current day at the top of the page 
 var showCurrentDay = function() {
     var currentDay = moment().format("dddd MMM Do");
     currentDayDisplay.append(currentDay);
 };
 
+// save button 
 $(".saveBtn").click(function() {
+    // gets the textarea value
     var text = $(this).siblings("textarea").val()
-    console.log(text);
+   
+    // gets the id of row
     var time = $(this).siblings("textarea").attr("id")
     console.log(time);
+    //save info to local storage
     localStorage.setItem(time, text)
   });
+
+  // loop through time rows and display localstorage info
  for ( i = 9; i < 18; i++) {
   $("#" + i).val(localStorage.getItem(i))
 };
+
+// checks time to set color 
  var checkTime = function() {
 $("textarea").each(function() {
-    var currentHour = moment("2015-01-16T12:00:00").format("hh:mm:ss a");
-    var time = $(this).attr("id") 
+    
+    var time = $(this).attr("id");
     time = parseInt(time)
 
     if (currentHour === time) {
@@ -29,15 +39,14 @@ $("textarea").each(function() {
     }
     else {
         $(this).addClass("past")
-    }
-    console.log(time);
-    
+    }   
 })
 }
 
 
-
+const interval = setInterval(function() {
+   checkTime();
+}, 5000);
 
 checkTime();
-
 showCurrentDay();
